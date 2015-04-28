cbbcc0e704549d32e041380b3383a5832b7acec4
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-04-28 19:44:12 +0930

    eyeul: Update build description.
    Change-Id: I936960080cc68411cd4b95e0984d5043cd304a1e

--- cm.mk
+++ cm.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 $(call inherit-product, device/htc/eyeul/full_eyeul.mk)
 
 # Enhanced NFC
@@ -7,3 +22,5 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_eyeul
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/cingular_us/htc_eyeul_att:4.4.4/KTU84P/396037.7:user/release-keys" PRIVATE_BUILD_DESC="1.15.502.7 CL396037 release-keys"
