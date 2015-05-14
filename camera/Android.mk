98b2c4a8ec41aa4b89fb56fe0eab301a22ee2cbb
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-05-14 10:29:26 +0930

    eyeul: stop camera conflicts.
    Change-Id: Ieb90c562775775167c602390919972675fbc833b

--- camera/Android.mk
+++ camera/Android.mk
@@ -11,7 +11,7 @@ LOCAL_C_INCLUDES += \
     system/media/camera/include
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.eye
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
