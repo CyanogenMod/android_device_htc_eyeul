98460f1d1af7a8830027b35680435136beea4785
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-06-29 17:07:04 +0930

    eyeul: Move back to new amp HAL
    this basically reverts "eye: move to old amp"
    
    This reverts commit 66c99140607bc16f537c9adad2af706cb3a72919.
    
    Change-Id: Ieecdd2c2b28f79c3757a3c2aeedf9c59c85b03c3

--- device.mk
+++ device.mk
@@ -75,6 +75,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
+    audio_amplifier.msm8974 \
     audio_policy.msm8974 \
     audio.primary.msm8974 \
     audio.r_submix.default \
@@ -84,9 +85,6 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     tinymix
-
-PRODUCT_PACKAGES += \
-    libaudioamp
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
