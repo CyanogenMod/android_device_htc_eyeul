3a8022c189356ac45b820e1e79f3212de62858ec
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-07-21 11:44:27 +0930

    eyeul: update bluetooth config.
    Change-Id: I3c4dbf4c087c9f9facc1f298636dd37d28288730

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QCOM_SW TRUE
+#define BLUETOOTH_QTI_SW TRUE
 
 #define BTM_DEF_LOCAL_NAME   "HTC Desire Eye"
 // Disables read remote device feature
