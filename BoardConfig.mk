4d4381f7dc3494c9fa09c20e53dd913e14689103
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-08-25 06:32:59 +0930

    eyeul: Rework tree for msm8974-common
    Change-Id: Iff3d277e396a901911587da9fd232688cc26c65e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,6 +26,10 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
+
+# Inherit from common msm8974-common
+-include device/htc/msm8974-common/BoardConfigCommon.mk
+
 # Model Ids
 # 0PFH10000 - AT&T/Dev Edition
 # 0PFH11000 - Europe/International
@@ -32,153 +37,22 @@
 
 TARGET_OTA_ASSERT_DEVICE := eyeul
 
-LOCAL_PATH := device/htc/eyeul
-
-BOARD_VENDOR := htc
-
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := MSM8974
-TARGET_NO_BOOTLOADER := true
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8974
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
-
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := krait
-
 # Kernel
-BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 zcache androidboot.bootdevice=msm_sdcc.1
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/eyeul/mkbootimg.mk
 TARGET_KERNEL_CONFIG := cm_eyeul_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/msm8974
-
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
-
-# Audio
-AUDIO_FEATURE_DISABLED_MULTI_VOICE_SESSIONS := true
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
-AUDIO_FEATURE_ENABLED_FM := true
-AUDIO_FEATURE_ENABLED_HWDEP_CAL := true
-AUDIO_FEATURE_ENABLED_LOW_LATENCY_CAPTURE := true
 
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/eyeul/bluetooth
 
-# Camera
-COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
-COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{"htc.camera.sensor.", AID_CAMERA, 0}, {"camera.4k2k.", AID_MEDIA, 0},'
-USE_DEVICE_SPECIFIC_CAMERA := true
-
-# Charge mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
-
-# Font
-EXTENDED_FONT_FOOTPRINT := true
-
-# Graphics
-TARGET_USES_ION := true
-USE_OPENGL_RENDERER := true
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
-TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
-
 # Includes
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
-# Libc extensions
-BOARD_PROVIDES_ADDITIONAL_BIONIC_STATIC_LIBS += libc_htc_symbols
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
+TARGET_SPECIFIC_HEADER_PATH := device/htc/eyeul/include
 
 # NFC
 BOARD_NFC_CHIPSET := pn547
 
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-
-# RPC
-TARGET_NO_RPC := true
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
-
-# Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_WLAN_DEVICE := qcwcn
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_FW_PATH_AP := "ap"
-
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4294967296
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 10200547328
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_RECOVERY_DEVICE_MODULES += chargeled
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 
-# Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_RECOVERY_SWIPE := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_USES_MMCUTILS := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-
-# SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/htc/eyeul/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    device.te \
-    file_contexts \
-    file.te \
-    hcheck.te \
-    init.te \
-    kernel.te \
-    mediaserver.te \
-    mm-qcamerad.te \
-    mpdecision.te \
-    platform_app.te \
-    property_contexts \
-    recovery.te \
-    radio.te \
-    rmt_storage.te \
-    system_server.te \
-    tap2wake_dev.te \
-    thermal-engine.te \
-    ueventd.te \
-    vibe_dev.te \
-    vold.te \
-    wpa.te
-
-# Hardware
-BOARD_HARDWARE_CLASS := device/htc/eyeul/cmhw
+# Inherit from the proprietary version
+-include vendor/htc/eyeul/BoardConfigVendor.mk
