f8b5cb63f40f8c708b3df541e9f345aafb089b4d
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-08-25 06:32:59 +0930

    eyeul: Rework tree for msm8974-common
    Getting close but hold!
    
    Change-Id: Iff3d277e396a901911587da9fd232688cc26c65e

--- cm.mk
+++ cm.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 
 $(call inherit-product, device/htc/eyeul/full_eyeul.mk)
 
@@ -23,4 +24,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_eyeul
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/htc_europe/htc_eyeul:5.0.2/LRX22G/504177.1:user/release-keys" PRIVATE_BUILD_DESC="2.19.401.1 CL504177 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="htc/htc_europe/htc_eyeul:5.0.2/LRX22G/504177.1:user/release-keys" \
+    PRIVATE_BUILD_DESC="2.19.401.1 CL504177 release-keys"
