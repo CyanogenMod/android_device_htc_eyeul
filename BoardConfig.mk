97600b7a96f069a30e5c8212efd391303c66ba9e
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-09-08 09:52:58 +0930

    eyeul: it's 2015
    Change-Id: Ia631c5cd9fa1be85500e51c7db642d546a0e2912

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 #
 # This file sets variables that control the way modules are built
@@ -19,6 +20,10 @@
 # included in a build is to use PRODUCT_PACKAGES in a product
 # definition file).
 #
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
 
 # Model Ids
 # 0PFH10000 - AT&T/Dev Edition
