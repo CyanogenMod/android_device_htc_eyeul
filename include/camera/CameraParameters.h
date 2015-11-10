66c45ca1e797df9223780218fb9bf340c17b8c80
Rouzbeh Ahmad <Rouzb3h@gmail.com>
2015-11-11 09:34:38 +1030

    eyeul: camera:  More parameters
    Change-Id: Ie4c7f9decb771268b06d2da204857486b9ffb522

--- include/camera/CameraParameters.h
+++ include/camera/CameraParameters.h
@@ -686,7 +686,12 @@ public:
     static const char LIGHTFX_HDR[];
 
     /* HTC parameters */
+    static const char KEY_CONTIBURST_TYPE[];
+    static const char KEY_CONTI_BURST_STATE[];
     static const char SCENE_MODE_TEXT[];
+    static const char CONTI_BURST_CAPTURING[];
+    static const char CONTI_BURST_CAPTURE_DONE[];
+    static const char KEY_FORCE_USE_AUDIO_ENABLED[];
     static const char KEY_SMILEINFO_BYFACE_SUPPORTED[];
 
     /**
